01a5b6fe812fef2770b776968db90c732a11bdcd
vicmcl <victorienmichel3@gmail.com>
2024-12-28 05:04:51 +0100

    FIX: set correctly the last month period when end_date < start_date + 1 month

--- tools/moving_window.py
+++ tools/moving_window.py
@@ -2,22 +2,16 @@ from dateutil.relativedelta import relativedelta
 from dateutil.parser import parse
 
 
+# function to split the date range into moving windows of 1 month
 def moving_window(start_date, end_date):
-    if start_date is not None and end_date is not None:
-        start, end = parse(start_date), parse(end_date)
-        window = []
-
-        while start < end:
-            previous_month = end - relativedelta(months=1)
-            if previous_month < start:
-                previous_month = start
-            window.append(
-                (
-                    str(previous_month.date().strftime("%d-%m-%Y")),
-                    str(end.date().strftime("%d-%m-%Y")),
-                )
-            )
-            end = previous_month
-        return window
-    else:
-        return [(None, None)]
+    start_date = parse(start_date, dayfirst=True)
+    end_date = parse(end_date, dayfirst=True)
+    while start_date < end_date:
+        if start_date + relativedelta(months=1) > end_date:
+            break
+        yield (
+            start_date.strftime("%d-%m-%Y"),
+            (start_date + relativedelta(months=1)).strftime("%d-%m-%Y"),
+        )
+        start_date = start_date + relativedelta(months=1)
+    yield (start_date.strftime("%d-%m-%Y"), end_date.strftime("%d-%m-%Y"))
