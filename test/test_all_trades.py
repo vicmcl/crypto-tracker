fd235071b658f594e3d16afce4e3550ee1c1c2ed
vicmcl <victorienmichel3@gmail.com>
2024-12-30 18:05:46 +0100

    FIX: update functions imported

--- test/test_all_trades.py
+++ test/test_all_trades.py
@@ -5,25 +5,30 @@ import sys
 sys.path.append("..")
 
 from tools.get_account_info import get_account_info
-from tools.parse_transactions import select_transaction_parser
+from tools.parse_transactions import transform_keys, parse_json, select_config
 from tools.send_request import send_signed_request, set_payload, get_endpoint
 
 
 def main():
-    tickers = list(get_account_info().index)
     transaction_type = "trade"
-    transaction_parser = select_transaction_parser(transaction_type)
     endpoint = get_endpoint(transaction_type)
+    key_config = select_config(transaction_type)
+    df = pd.DataFrame()
 
     try:
-        df = pd.DataFrame()
+        tickers = [t for t in list(get_account_info().index) if "USD" not in t]
         for ticker in tickers:
             for stablecoin in ["USDT", "USDC"]:
                 symbol = ticker + stablecoin
                 params = set_payload(endpoint, symbol=symbol)
                 response = send_signed_request("GET", endpoint, params)
-                df = pd.concat([df, pd.DataFrame(transaction_parser(response))])
-        df.to_csv(f"../trades.csv", index=False)
+                if "code" in response:
+                    print(f"Error: {response['msg']}")
+                    continue
+                transactions = parse_json(response, transaction_type)
+                filtered_transactions = transform_keys(transactions, key_config)
+                df = pd.concat([df, pd.DataFrame(filtered_transactions)])
+        df.to_csv(f"../csv/trades.csv", index=False)
 
     except requests.exceptions.RequestException as e:
         print(f"Error pinging Binance API: {e}")
